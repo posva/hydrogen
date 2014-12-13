7bb2862cd1f50962c2c03352fdb5b9b691515bc1
Eduardo San Martin Morote <eduardo.san-martin-morote@ensimag.grenoble-inp.fr>
2014-12-13 23:46:08 +0100

    trying to fix audio

--- sounds.js
+++ sounds.js
@@ -1,8 +1,18 @@
 var sounds = {
-  'solid'					: new Audio('sound/solid.ogg'),
-  'liquid'				: new Audio('sound/liquid.ogg'),
-  'gas'					: new Audio('sound/gas.ogg'),
-  'music'					: new Audio('sound/music.ogg')
+  'solid'      : new Audio('sound/solid.ogg'),
+  'liquid'     : new Audio('sound/liquid.ogg'),
+  'gas'        : new Audio('sound/gas.ogg'),
+  'music'      : new Audio('sound/music.ogg')
+};
+var soundsSrc = {
+  'solid'  : 'sound/solid.ogg',
+  'liquid' : 'sound/liquid.ogg',
+  'gas'    : 'sound/gas.ogg',
+  'music'  : 'sound/music.ogg'
+};
+sounds.music.loop = true; // doesn't work everywhere??
+var reloadOnStop = function() { // needed in order to play again the audio
+  this.load();
 };
 var soundAvailable = {};
 var totalSounds = 0, loadedSounds = 0;
@@ -24,11 +34,20 @@ for (var k in sounds) {
   if (sounds.hasOwnProperty(k)) {
     totalSounds++;
     sounds[k].addEventListener('loadeddata', soundOnLoad);
+    if (k !== 'music')
+      sounds[k].addEventListener('ended', reloadOnStop.bind(sounds[k]));
+    sounds[k].src = soundsSrc[k];
+    sounds[k].load();
   }
 }
+sounds.music.addEventListener('ended', function() {
+  sounds.music.load();
+  sounds.music.play();
+});
 
 function Sound(name) {
   this.sound = sounds[name];
+  this.sound.pause();
   this.sound.currentTime = 0;
   this.sound.play();
 }
